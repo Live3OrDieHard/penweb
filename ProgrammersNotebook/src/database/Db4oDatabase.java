30bed70f373c88cb348253ce2e26a3c6073bc961
Andy Creeth <andy@wpi.edu>
2012-10-31 16:38:29 -0400

    Added comment explaing how to use Db4oDatabase.

--- ProgrammersNotebook/src/database/Db4oDatabase.java
+++ ProgrammersNotebook/src/database/Db4oDatabase.java
@@ -7,6 +7,17 @@ import com.db4o.query.*;
 import dataStructure.IEntry;
 import dataStructure.IHeader;
 
+/*
+ * How to create a new instance of this database using IDatabase:
+ * IDatabase db = new Db4oDatabase("db/localDb.yap");
+ * 
+ * Now you can store and query
+ * db.store(anEntry);
+ * List<IEntry> entries = db.getAll();
+ * 
+ * If you have any questions about how to use this ask Andy C <andy@wpi.edu>
+ */
+
 public class Db4oDatabase implements IDatabase {
 
 	private ObjectContainer db;
