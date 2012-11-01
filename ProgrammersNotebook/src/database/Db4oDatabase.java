7a91af0503dddadd328fb0717ef4f91128d14c8f
mbrigham1223 <mbrigham1223@wpi.edu>
2012-11-01 11:00:56 -0400

    I implemented the getByHeader function, but did not test it. I wasn't confident on how.

--- ProgrammersNotebook/src/database/Db4oDatabase.java
+++ ProgrammersNotebook/src/database/Db4oDatabase.java
@@ -6,6 +6,7 @@ import com.db4o.query.*;
 
 import dataStructure.IEntry;
 import dataStructure.IHeader;
+import dataStructure.IPerson;
 
 /*
  * How to create a new instance of this database using IDatabase:
@@ -38,11 +39,42 @@ public class Db4oDatabase implements IDatabase {
 
 	@Override
 	public List<IEntry> getByHeader(final IHeader head) {
-		return db.query(new Predicate<IEntry>() {
-			public boolean match(IEntry e) {
-				return e.getHeader().getTitle().equals(head.getTitle());
-			}
-		});
+		boolean hasTitle = true;
+		boolean hasAuthors = true;
+		final String title = head.getTitle();
+		final List<IPerson> authors = head.getAuthors();
+		
+		if (title.equals(null)) {
+			hasTitle = false;
+		}
+		
+		if (authors.equals(null)) {
+			hasAuthors = false;
+		}
+		
+		// All authors searches only work if lists are ordered the same.
+		if (hasTitle && hasAuthors) {
+			return db.query(new Predicate<IEntry>() {
+				public boolean match(IEntry e) {
+					return (e.getHeader().getTitle().equals(title) && e.getHeader().getAuthors().equals(authors));
+				}
+			});
+		}
+		else if (hasTitle) {
+			return db.query(new Predicate<IEntry>() {
+				public boolean match(IEntry e) {
+					return e.getHeader().getTitle().equals(head.getTitle());
+				}
+			});
+		}
+		else if (hasAuthors) {
+			return db.query(new Predicate<IEntry>() {
+				public boolean match(IEntry e) {
+					return e.getHeader().getAuthors().equals(authors);
+				}
+			});
+		}
+		else return db.query(IEntry.class);
 	}
 
 	@Override
