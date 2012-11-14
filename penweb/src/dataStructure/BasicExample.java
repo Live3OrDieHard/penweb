f7821298d698f2b9c060ceea03205cae5d24ec88
Justin Chines <jfchines@wpi.edu>
2012-11-14 17:56:00 -0500

    Implement the getAuthorsNames method in BasicExample

--- penweb/src/dataStructure/BasicExample.java
+++ penweb/src/dataStructure/BasicExample.java
@@ -123,8 +123,21 @@ public class BasicExample implements IExample {
 	}
 
 	@Override
+	/**
+	 * Perhaps this should be moved into the controller. 
+	 * I don't know how I feel about functions with a lot of logic
+	 * like this in the dataStructure classes
+	 */
 	public String getAuthorsNames() {
-		return null;
+		String nameList = "";
+		
+		for (int i = 0; i < authors.size(); ++i) {
+			nameList += authors.get(i).getDisplayName();
+			if (i < authors.size() - 1)
+				nameList += ", ";
+		}
+		
+		return nameList;
 	}
 
 	@Override
