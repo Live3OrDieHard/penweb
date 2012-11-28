648c071712c24aebd01ac00b7312355a8d2bf2d8
andy <awiovanna@wpi.edu>
2012-11-28 15:27:34 -0500

    Added copyright notice to every class.

--- penweb/src/database/Db4oDatabase.java
+++ penweb/src/database/Db4oDatabase.java
@@ -1,5 +1,18 @@
+/*******************************************************************************
+ * Copyright (c) 2012 Team 3: Live Three or Die Hard
+ * 
+ * All rights reserved. This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors: Team 3
+ *******************************************************************************/
+
 package database;
 
+/**
+ * 
+ */
 import java.util.ArrayList;
 import java.util.List;
 
--- penweb/src/test/ExampleAndCategoryTest.java
+++ penweb/src/test/ExampleAndCategoryTest.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2012 Team 3: Live Three or Die Hard
+ * 
+ * All rights reserved. This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors: Team 3
+ *******************************************************************************/
+
 package test;
 
 /**
