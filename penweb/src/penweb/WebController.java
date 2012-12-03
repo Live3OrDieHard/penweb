0320c5fd949190397f34f11e19dd0b20e0dd1b32
Peng Ren <pren@wpi.edu>
2012-12-03 18:39:41 -0500

    implement remove dependency functionality in delete method in webcontroller

--- penweb/src/penweb/WebController.java
+++ penweb/src/penweb/WebController.java
@@ -403,7 +403,7 @@ public class WebController {
 			//TODO: Should we remove if the example is a dependency for another?
 			//Check story "Delete a code example from a public notebook"
 			if(getDependerOf((IExample) entry).size()!=0)
-				return 2;
+				removeAllDependency((IExample) entry);
 		} 
 		else if(entry instanceof IUser) {
 			// TODO Allow deletion for users
